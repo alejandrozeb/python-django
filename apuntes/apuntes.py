33f84c7993f1881e33cd4a2d07b3bce906b40c8c
unknown <alejandrojzeballos@gmail.com>
2021-02-05 01:44:21 -0400

    añadiedndo las extensiones

--- apuntes/apuntes.py
+++ apuntes/apuntes.py
@@ -3,4 +3,14 @@ Curso python apuntes
 https://www.python.org/downloads/
 
 descargamos (el sitio reconocera el sistema operativo) e instalamos marcando el check de agregar al path.
+
+extensiones para vs
+python
+python for VsCOde
+python exyension Pack
+Python extended
+python Indent
+Arelp for python
+Python Preview
+code runner
  """
