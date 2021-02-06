21be44218eb86def680afc7d98a040a4a4e4b0f1
unknown <alejandrojzeballos@gmail.com>
2021-02-06 02:00:52 -0400

    variables en memoria python

--- curso/fundamentos/variables.py
+++ curso/fundamentos/variables.py
@@ -1,6 +1,11 @@
 ''' es informacion que se va a guardar en la memoria de la pc '''
 x = 3
 print(x)
+
+y=3
+z = x + y
+print(z)
 print(x)
-print(x)
-print(x)
+print(y)
+
+""" en memoria, tenemos celdas donde la primera tiene el valor de 3 otra de 5 y otra de z con punteros de referencia a los valores """
