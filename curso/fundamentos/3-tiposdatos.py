fad331138a65b3af7bedac78957f13fc1d18e315
unknown <alejandrojzeballos@gmail.com>
2021-02-07 01:33:29 -0400

    {Add} tipos de datos en python

---
+++ curso/fundamentos/3-tiposdatos.py
@@ -0,0 +1,17 @@
+x=5
+""" x es de tipo entero """
+y=10.1
+""" tipo flotante """
+z=False
+""" tipo boolean, python es sensible a las mayusculas False es distinto de false """
+
+print(x)
+print(y)
+print(z)
+
+""" podemos el tipo de variable(literal) con type(variable) pero al igual que id solo se puede ejecutar en el interprete de python, en la consola propia de python """
+
+
+a="Saludo"
+""" tipo String es un conjunto de caracteres """
+print(a)
