8dd36a89589d36b9077efb750d4bbeb973fcd7bb
unknown <alejandrojzeballos@gmail.com>
2021-02-07 01:55:12 -0400

    {add} tipo de dato string u cadena

---
+++ curso/fundamentos/5-tipostring.py
@@ -0,0 +1,19 @@
+""" nos ayuda en busquedas de datos que a diario usamos en una busqueda de google """
+cadena = "aerosmith"
+print(cadena)
+""" tipo de dato string """
+print("Mi grupo favorito es: " + cadena)
+""" se concatena con + tambien puede ser entre variables """
+numero1="1"
+numero2="2"
+print(numero1 + numero2)
+""" pero tambien debemos tomar en cuenta la diferencia entre contenar dos variables tipo y string y sumar dos variables de tipo numerico, en este caso concatenamos dos string y el resultado es 12 """
+
+num1=1
+num2=2
+print(num1+num2)
+""" suma de tipo numerico """
+print("Opracion suma:", num1+num2)
+""" pero como parte del metodo print podemo concatenar una cadena con una variable u operación de tipo numerico """
+print("Opracion suma:" +str(num1+num2))
+""" tambien podemos convertir el resultado en uno de tipo string o cadena cualquier opción es valida"""
