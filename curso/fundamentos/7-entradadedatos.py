539687bb62aa18a778cf6076ece30046bf6bdc99
unknown <alejandrojzeballos@gmail.com>
2021-02-08 01:37:16 -0400

    {Add} entrada de datos en python

---
+++ curso/fundamentos/7-entradadedatos.py
@@ -0,0 +1,20 @@
+''' para que el usuario pueda ingresar datos '''
+
+resultado = input()
+print(resultado)
+num1= int(input()) #convertimos la entrda en numero pero la entrada debe ser un int
+num2= int(input())
+print(num2)
+
+print(num1+num2)
+numero1= input()
+numero2= input()
+print(numero1 + numero2)#concatena los datos como string
+
+''' para ejecutar el archivo puedes usar la consola de python y copiar el codigo,
+o abrir un consola cmd o powershell y ejecutar py (nombre del archivo con la extension)
+ej py 7-entradadedatos.py
+
+y se ejecutara este archivo
+'''
+''' en vs podemos abrir una terminal python en la terminal '''
