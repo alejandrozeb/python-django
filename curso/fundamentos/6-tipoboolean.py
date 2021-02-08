293588d8fd9d106815ade54a20d59167a2921009
unknown <alejandrojzeballos@gmail.com>
2021-02-08 01:28:47 -0400

    {Add} agrgando el tipo de dato boolean y su uso

---
+++ curso/fundamentos/6-tipoboolean.py
@@ -0,0 +1,26 @@
+''' representa un valor de verdadero o falso '''
+
+x=True
+print(x)
+
+y=False
+print(y)
+
+''' usaremos en decisiones que debamos tomar al programar'''
+
+num1=1
+num2=2
+resultado = num1 > num2
+print(resultado)
+resultado = num1 < num2
+print(resultado)
+
+if(num1 < num2):
+    print("el valor num1 es menor que num2")
+else:
+    print("el valor num1 No es menor al num2")
+
+
+
+''' comentarios en bloque  '''
+#comentario de una sola linea
