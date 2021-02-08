97ca3ef86652fea9c2f2e51c1fe08b71f2112ae8
unknown <alejandrojzeballos@gmail.com>
2021-02-08 01:57:02 -0400

    {Add} operadores aritmeticos

---
+++ curso/fundamentos/8-operadoresaritmeticos.py
@@ -0,0 +1,17 @@
+a = 3
+b = 2
+
+suma = a+b
+print("suma:",suma)
+resta = a-b
+print("resta:", resta)
+multiplicacion=a*b
+print("multiplicacion",multiplicacion)
+division=a/b
+print("division", division)
+modulo= a%b #es el residuo de la division
+print("modulo:",modulo)
+exponente = a**b
+print("exponente: ",exponente)
+
+''' tambien podemos usar input para asignar las variables '''
