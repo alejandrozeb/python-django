e46cc232de47661382405efe0121a70cdb49adbc
unknown <alejandrojzeballos@gmail.com>
2021-02-08 02:02:05 -0400

    {Add} operadores de asignacion en python

---
+++ curso/fundamentos/9-operadoresdeasignacion.py
@@ -0,0 +1,15 @@
+'''  '''
+x=3
+print(x)
+
+#sumar un valor en la asignacion
+x += 2  #x= x+2
+print(x)
+
+''' podemos hacerlo en cualquier operacion '''
+x -= 2  
+print(x)
+x *= 3  
+print(x)
+x /= 2  
+print(x)
