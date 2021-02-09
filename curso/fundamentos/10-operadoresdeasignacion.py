4c3a32a5a721052c30bbadcc20c7e7726ae61f75
unknown <alejandrojzeballos@gmail.com>
2021-02-09 01:46:03 -0400

    {add} operadores de aignacion en python

---
+++ curso/fundamentos/10-operadoresdeasignacion.py
@@ -0,0 +1,36 @@
+#if
+a=3
+b=2
+resultado = a == b  #igualdad
+print(resultado)
+
+resultado = a != b  #diferentes
+print(resultado)
+
+resultado = a > b  #mayor
+print(resultado)
+
+resultado = a >= b  #mayorigual
+print(resultado)
+
+resultado = a < b  #menor
+print(resultado)
+
+resultado = a <= b  #menor igual
+print(resultado)
+
+''' modulo de dos o par '''
+if a%2 == 0:
+    print("es par")
+else:
+    print("Es impar")
+#el modulo devuelvuel de residuo de la division
+
+edadLimite= 18
+edadPersona = 5
+if(edadPersona >= edadLimite):
+    print("es un adulto")
+else:
+    print("es menor de edad")
+    
+''' podemos ejecutar paso a paso cada instruccion y variar lo valores de las variables para tener un mejor entendimiento del funcionamiento de cada operador '''
