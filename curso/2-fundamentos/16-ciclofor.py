04c944f112f729061b919aa03911a6a0dad52cb5
unknown <alejandrojzeballos@gmail.com>
2021-02-17 00:02:08 -0400

    {add} ciclo for

---
+++ curso/2-fundamentos/16-ciclofor.py
@@ -0,0 +1,7 @@
+""" se usa mayormente como u iterador de elementos como strings o arrays """
+
+for letra in "Hola":
+    print(letra)
+else:
+    print("fin Ciclo for")
+#itera toda la cadena hola con indice inicial 0
