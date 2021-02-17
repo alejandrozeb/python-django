7c53bb6650aded6ee1cc0efb550d92b0d721ccd0
unknown <alejandrojzeballos@gmail.com>
2021-02-16 23:52:46 -0400

    {add} ciclo while formas de condicionales

---
+++ curso/2-fundamentos/15-ciclowhile.py
@@ -0,0 +1,17 @@
+""" nos permite ejecutar una sentencia las veces que queramos siempre y cuando cumpla con una condicion """
+
+condition = False
+
+while condition:
+    print("ejecutando ciclo while")
+else:
+    print ("Fin del ciclo while")   #entra cunado la condicion es falsa
+    
+#contador
+i = 0
+while i < 3:
+    print(i)
+    i += 1 #todo lo tabulado ejecutara el ciclo while 
+else:
+    print("Fin ciclo while")
+
