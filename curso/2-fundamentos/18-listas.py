e320cb76bd553f2a207eb9ff7f3782a2a5691e14
unknown <alejandrojzeballos@gmail.com>
2021-02-20 00:37:34 -0400

    {add} Introduccion a listas

---
+++ curso/2-fundamentos/18-listas.py
@@ -0,0 +1,15 @@
+""" es un conjuntod e elemtnos que podeoms guardar una variables"""
+
+nombres = ['Juan', "Karla", "Maria"] #lista de tipos string
+#el indice comienza por el 0 y terminan en n-1 (0.1.2.3)
+print(nombres)
+
+#largo de la lista
+print(len(nombres))
+
+#acceder a un elemento, el primer elemento
+print(nombres[0])
+print(nombres[1])
+
+#tambine podemos acceder de forma inversa con indices negativas
+print(nombres[-1])
