392f0f9f59c5c5c09c666140635abd74f44e4e5b
unknown <alejandrojzeballos@gmail.com>
2021-02-24 02:03:56 -0400

    {add} manejo de argumentos con parametros en funciones

---
+++ curso/2-fundamentos/23-funcionesconargumentos.py
@@ -0,0 +1,12 @@
+# Parametro es una variable definida entre los parentesis de la funcion
+
+#Argumento (arg) es el valor enviado a la funcion
+
+def funcion_arg(nombre, apellido):
+    print("el nombre recibido es: ", nombre)
+    print("el apellido recibido es: ", apellido)
+
+funcion_arg("Alejnadro", "Zeballos") # si no enviamos un argumento sale un error
+
+funcion_arg("Karla", "Gomez")
+
