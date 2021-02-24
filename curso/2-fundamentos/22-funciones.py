77f1de5c2bbf16c8365935ca9a07b41db49ac02b
unknown <alejandrojzeballos@gmail.com>
2021-02-24 01:54:38 -0400

    {add} uso de funciones

---
+++ curso/2-fundamentos/22-funciones.py
@@ -0,0 +1,10 @@
+''' funciones es un bloque de codigo que debe ser llamado para ser ejecutado  '''
+
+def mi_funcion():
+    print("Ejecutando mi funcion")
+    
+
+mi_funcion()#no tiene argumentos, se ejecuta aqui 
+#podemos reutilizar codigo porque podemos llamarla muchas veces
+mi_funcion()
+mi_funcion()
