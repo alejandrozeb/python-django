78a73363b878508ac77d1f8376fb162f6acbfb82
unknown <alejandrojzeballos@gmail.com>
2021-02-24 02:10:03 -0400

    {add} uso del return y valores por defecto en funciones

---
+++ curso/2-fundamentos/24-funcionesreturn.py
@@ -0,0 +1,10 @@
+
+def suma(a = 0, b = 0):
+    return a + b
+
+print("El resultado es: ",suma(5,3))
+#con return podemos devolver valores que psaron por un proceso o no como resultado de la funcion que definimos
+
+#es el valor regresado por la llamada de la funcion
+
+# si no pasamos argumetnos sale un error o podemos definir valores por defecto de la funcion
