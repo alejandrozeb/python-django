c1b05167633431e7a9f2e97f4d4aeb794a1aeb90
unknown <alejandrojzeballos@gmail.com>
2021-03-09 23:50:06 -0400

    {add} Instancia de objetos

--- curso/2-fundamentos/25-persona.py
+++ curso/2-fundamentos/25-persona.py
@@ -15,3 +15,20 @@ Persona.edad = 85
 print(Persona.nombre, " ", Persona.edad)
 #la identacion es exactamente igual a los bloques de codigo
 #python esta creando el objeto de manera implicita
+
+#Creando un objeto
+persona = Persona("Alejandro", 90)
+
+print(persona.nombre)
+print(persona.edad)
+print(id(persona))#direccion de memoria
+
+#se debe crear objetos a partir del constructor
+
+#creacion de un segundo objeto
+persona2 = Persona('Alejandro2', 25)
+print(persona2.nombre)
+print(persona2.edad)
+print(id(persona2))#direccion de memoria
+
+#los dos objetos se encuentran en una diferente direccion de memoria, estos objetos son independientes con sus  propios punteros
